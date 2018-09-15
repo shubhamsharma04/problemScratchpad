ca394f265bd02f9b2ce6c674c35e760f7d707d6c
opensam <opensam4@gmail.com>
2018-09-15 16:06:24 -0700

    removed unused imports

--- src/test/java/com/opensam/problem/scratchpad/problems/SearchAndSortTests.java
+++ src/test/java/com/opensam/problem/scratchpad/problems/SearchAndSortTests.java
@@ -1,15 +1,9 @@
 package com.opensam.problem.scratchpad.problems;
 
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
 @RunWith(SpringRunner.class)
 public class SearchAndSortTests {
